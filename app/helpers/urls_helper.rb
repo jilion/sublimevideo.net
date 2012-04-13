a0a91d29a8dbe22c17100ff59708d845457d0115
Rémy Coutable <remy@jilion.com>
2012-04-13 16:32:37 +0200

    Include layout new code into this app until next version is out...

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -1,12 +1,12 @@
 module UrlsHelper
-  
+
   def demo_site_stats_url
-    "#{https_if_prod_or_staging}://my.#{request.domain}/sites/stats/demo"
-  end
-  
-  def https_if_prod_or_staging
-    Rails.env.production? || Rails.env.staging? ? 'https' : 'http'
+    my_url('sites/stats/demo')
   end
 
-  
+  # lame hack until sublimevideo_layout 0.6.6 is released...
+  def sublimevideo_include_tag(ssl_request, name)
+    %(<script src="#{ssl_request ? "https://4076.voxcdn.com" : "http://cdn.sublimevideo.net"}/js/#{::SiteToken[name.to_sym]}.js" type="text/javascript"></script>)
+  end
+
 end

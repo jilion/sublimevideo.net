a901dbacb440df45e410c61742788684145ff94e
Rémy Coutable <remy@jilion.com>
2012-06-19 11:12:42 +0200

    Rack::Cache is not present in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,7 +3,7 @@ SublimeVideo::Application.configure do
   config.middleware.insert_after Rack::Lock, "::Rack::Auth::Basic", "Staging" do |u, p|
     [u, p] == ['jilion', ENV['PRIVATE_CODE']]
   end
-  config.middleware.insert_before Rack::Cache, Rack::SslEnforcer, except_hosts: 'sublimevideo-staging.net', strict: true
+  config.middleware.insert_before Rack::Lock, Rack::SslEnforcer, except_hosts: 'sublimevideo-staging.net', strict: true
   config.middleware.insert_before Rack::SslEnforcer, Rack::NoWWW
 
   # The production environment is meant for finished, "live" apps.

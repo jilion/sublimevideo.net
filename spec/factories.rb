97551b303b0cf54e9d908dc22d858dfc1f249639
Rémy Coutable <remy@jilion.com>
2012-12-04 18:33:25 +0100

    Require the company name in the tailor-made player request form

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory :tailor_made_player_request do
     name        'John Doe'
     email       'john@doe.com'
+    company     'Foo & Co.'
     topic       'agency'
     description 'Want a player.'
   end

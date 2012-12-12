9a000ad57c29d7b44c09d068909c92342fe28f95
Zeno Crivelli <zeno@zeno.name>
2012-12-12 11:31:22 -0800

    Update press-kit link, again

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ SublimeVideo::Application.routes.draw do
   end
 
   get '/pr/:page' => 'press_releases#show', as: :pr, constraints: PRConstraint, format: false
-  get '/press-kit' => redirect('http://cl.ly/0V1H2p2K320A/content'), as: :press_kit
+  get '/press-kit' => redirect('http://cl.ly/0g0u032K3p26/content'), as: :press_kit
 
   get '/:page' => 'pages#show', as: :page, constraints: PagesConstraint, format: false
 

6dd98b51f6ed458d0455aeb55496213e0f02ffee
Octave Zangs <octavez@octavez.com>
2012-12-14 17:32:49 +0100

    Updated Press Kit Link

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ SublimeVideo::Application.routes.draw do
   end
 
   get '/pr/:page' => 'press_releases#show', as: :pr, constraints: PRConstraint, format: false
-  get '/press-kit' => redirect('http://cl.ly/0g0u032K3p26/content'), as: :press_kit
+  get '/press-kit' => redirect('http://cl.ly/2G0U46132f2x/content'), as: :press_kit
 
   get '/:page' => 'pages#show', as: :page, constraints: PagesConstraint, format: false
 

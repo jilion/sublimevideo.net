a2fbfa53b1fdd02dbf94dd01dbeced2285de2b08
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-12-14 12:00:56 +0100

    http:// for asset_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ SublimeVideo::Application.configure do
   }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "//cdn.sublimevideo.net"
+  config.action_controller.asset_host = "http://cdn.sublimevideo.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )

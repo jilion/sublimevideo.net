c0a84f42bfdada4b6671a8001e8ce5d717d6e953
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-01-22 15:42:36 +0100

    config.assets.initialize_on_precompile => false
    Use ENV variable for asset_sync configuration

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,13 @@ module SublimeVideo
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
+    # http://guides.rubyonrails.org/asset_pipeline.html#precompiling-assets
+    # For faster asset precompiles, you can partially load your application
+    # by setting config.assets.initialize_on_precompile to false
+    # in config/application.rb, though in that case templates cannot see
+    # application objects or methods. Heroku requires this to be false.
+    config.assets.initialize_on_precompile = false
+
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 

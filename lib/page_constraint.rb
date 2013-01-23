61dc7bbc1726e9ae22d7fff2a1218d3c88c722d5
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-01-23 15:43:19 +0100

    Refactorize PageConstraint
    Thanks @rymai !

--- lib/page_constraint.rb
+++ lib/page_constraint.rb
@@ -1,14 +1,11 @@
 class PageConstraint
   def self.matches?(request)
-    pages = Dir.glob("app/views/#{resources}/*.html.haml").map { |page|
-      page.match(%r(app/views/#{resources}/(.*)\.html\.haml))[1]
-    }
-    pages.include?(request.params["page"])
+    Dir.glob("app/views/#{resources}/#{request.params["page"]}.html.haml").any?
   end
 
   private
 
   def self.resources
-    self.to_s.underscore.gsub(/_constraint/, '')
+    self.to_s.underscore.gsub(/_constraint$/, '')
   end
 end

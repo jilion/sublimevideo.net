5df83a9d67cc990b0ac9a0abb98796ef6f2252e4
Octave Zangs <octavez@octavez.com>
2013-04-24 11:49:52 +0200

    Start new Features + Pricing page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ SublimeVideo::Application.routes.draw do
   end
 
   # Redirects
-  %w[demo features].each { |action| get action => redirect('/modular-player') }
+  %w[demo modular-player].each { |action| get action => redirect('/features') }
   %w[signup sign_up register].each { |action| get action => redirect('/?p=signup') }
   %w[login log_in sign_in signin].each { |action| get action => redirect('/?p=login') }
   get 'why' => redirect('/horizon-framework')

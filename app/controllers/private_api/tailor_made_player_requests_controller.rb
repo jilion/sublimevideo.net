50d16201fc05a3f47168be7d70cc631802d61562
Rémy Coutable <remy@rymai.me>
2013-05-17 12:09:27 +0200

    Try to fix private API controller

--- app/controllers/private_api/tailor_made_player_requests_controller.rb
+++ app/controllers/private_api/tailor_made_player_requests_controller.rb
@@ -3,8 +3,7 @@ class PrivateApi::TailorMadePlayerRequestsController < SublimeVideoPrivateApiCon
   before_filter :find_tailor_made_player_request, only: [:show, :destroy]
 
   def index
-    @tailor_made_player_requests = apply_scopes(TailorMadePlayerRequest.scoped)
-    @tailor_made_player_requests = @tailor_made_player_requests.page(params[:page])
+    @tailor_made_player_requests = apply_scopes(TailorMadePlayerRequest.page(params[:page]))
 
     respond_with(@tailor_made_player_requests)
   end

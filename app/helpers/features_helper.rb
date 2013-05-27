82bbb16e2fe3f184a8eeb1868b804367d878816e
Octave Zangs <octavez@octavez.com>
2013-05-27 18:15:40 +0200

    Features page progression

--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -1,9 +1,19 @@
 module FeaturesHelper
 
   def feature(feature, opts = {})
-    options = { demo: true, learn_more_url: nil }.merge(opts)
+    options = { feature: feature, demo: true, learn_more_url: nil, pricing: '' }.merge(opts)
 
-    render 'features/feature', feature: feature, demo: options[:demo], learn_more_url: options[:learn_more_url]
+    render 'features/feature', options
+  end
+
+  def add_on(feature, opts = {})
+    feature(feature, opts.merge(pricing: render("features/pricing/#{feature}")))
+  end
+
+  def display_amount_with_sup(amount_in_cents)
+    units = amount_in_cents.to_i / 100
+    decimals = amount_in_cents.to_i - (units * 100)
+    "#{number_to_currency(units, precision: 0)}#{content_tag(:sup, ".#{decimals}") unless decimals.zero?}".html_safe
   end
 
 end

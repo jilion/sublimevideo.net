0491ca39c7188400d686752ae03459e806f1627c
Octave Zangs <octavez@octavez.com>
2013-05-28 12:35:17 +0200

    Features page improvements to display add-on subscribe form

--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -1,13 +1,13 @@
 module FeaturesHelper
 
   def feature(feature, opts = {})
-    options = { feature: feature, demo: true, learn_more_url: nil, pricing: '' }.merge(opts)
+    options = { feature: feature, demo: true, learn_more_url: nil, add_ons: '' }.merge(opts)
 
     render 'features/feature', options
   end
 
   def add_on(feature, opts = {})
-    feature(feature, opts.merge(pricing: render("features/pricing/#{feature}")))
+    feature(feature, opts.merge(add_ons: render('features/add_ons', add_on_category: feature, add_ons: opts.delete(:add_ons))))
   end
 
   def display_amount_with_sup(amount_in_cents)

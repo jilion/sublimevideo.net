bc03153f6e3a9fa8abf8c6ceed51caa7a288300f
Octave Zangs <octavez@octavez.com>
2013-06-19 15:07:13 +0200

    Mini header for demo page

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -2,6 +2,7 @@ class DemosController < ApplicationController
   include CachedPageRendererControllerHelper
 
   def show
+    @header_class = 'l-mini'
     params[:page] = [params[:feature], params[:demo]].join('/')
     render_cached_page
   end

141445309b4e364f446d531ee61245aa88cdd1e3
Rémy Coutable <remy@rymai.me>
2013-09-10 12:02:55 +0200

    Tweak RSpec config

--- spec/config/rspec.rb
+++ spec/config/rspec.rb
@@ -1,8 +1,9 @@
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run_including focus: true
+  config.filter_run_including focus: ENV['CI'] != 'true'
   config.mock_with :rspec
+  config.fail_fast = ENV['CI'] != 'true'
 
   config.before :all, type: :request do
     host! 'sublimevideo.dev'
